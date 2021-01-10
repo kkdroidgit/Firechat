38192e0f2358e511ef8f03b3d9cff56e6391566a
kkdroidgit <kartikdroid17@gmail.com>
2021-01-10 19:35:26 +0530

    firebase configured

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import {userAuthState} from 'react-firebase-hooks/auth';
+import {userCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDQTF0BO156AS1udLc-Ltc9sozJG_eDMu8",
+  authDomain: "firechat-6ca8d.firebaseapp.com",
+  projectId: "firechat-6ca8d",
+  storageBucket: "firechat-6ca8d.appspot.com",
+  messagingSenderId: "11335683357",
+  appId: "1:11335683357:web:d4661b20207c9e982ae5f4",
+  measurementId: "G-FEPCFN04VF"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
